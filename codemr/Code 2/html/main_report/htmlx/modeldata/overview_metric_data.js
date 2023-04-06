9fbd153ec14f13fa202a6d585dd9b26056de1147
Dnbhatt <62647174+Dnbhatt99@users.noreply.github.com>
2023-04-05 22:04:16 -0500

    Assigment 4

---
+++ codemr/Code 2/html/main_report/htmlx/modeldata/overview_metric_data.js
@@ -0,0 +1,1 @@
+var CodeMR_ProjectMetrics = {"Project Name":"Code", "Number of Highly Problematic Classes":11, "Number of Entities":1088, "Number of Problematic Classes":159, "Class Lines of Code":78374, "Number of External Packages":64, "Number of Packages":35, "Number of External Entities":772};var EQ_COLOR_OF_LEVELS = ["#1F77B4","#007F24","#62BF18","#FFC800","#FF5B13","#E50000"];
---
+++ codemr/Code/html/main_report/htmlx/modeldata/overview_metric_data.js
@@ -0,0 +1,1 @@
+var CodeMR_ProjectMetrics = {"Project Name":"Code", "Number of Highly Problematic Classes":11, "Number of Entities":1146, "Number of Problematic Classes":164, "Class Lines of Code":83963, "Number of External Packages":69, "Number of Packages":41, "Number of External Entities":790};var EQ_COLOR_OF_LEVELS = ["#1F77B4","#007F24","#62BF18","#FFC800","#FF5B13","#E50000"];
